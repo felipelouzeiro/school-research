15185dc1b59c8a6e789cbd2b3031b3f58a9d3f4a
felipe <fl.louzeiro@gmail.com>
2022-08-31 19:07:36 -0300

    ✨ feat: configurando rotas com react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-export const App = () => {
-  return <div>App</div>;
-};
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './components/home';
 
-export default App;
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/favorites" element={<h1>Favorites</h1>} />
+      <Route path="/search" element={<h1>Search</h1>} />
+      <Route path="*" element={<h1>Page not found</h1>} />
+    </Routes>
+  );
+};
